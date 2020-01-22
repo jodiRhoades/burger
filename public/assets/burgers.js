c38f486c3f5a6384365af6165fce8d1bf349201a
Jodi Rhoades <jo_kawgirl@yahoo.com>
2020-01-21 21:40:23 -0500

    connection to heroku

--- public/assets/burgers.js
+++ public/assets/burgers.js
@@ -25,10 +25,10 @@ $(function() {
       // Make sure to preventDefault on a submit event.
       event.preventDefault();
   
-      var newBurger = {
-        name: $("#ca").val().trim(),
-        devoured: $("[name=devoured]:checked").val().trim()
-      };
+    //   var newBurger = {
+    //     name: $("#ca").val().trim(),
+    //     devoured: $("[name=devoured]:checked").val().trim()
+    //   };
   
       // Send the POST request.
       $.ajax("/api/burgers", {
